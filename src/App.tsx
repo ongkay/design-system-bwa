f3c477dcbcd09519ea9c734fb5fcffdd64ca42d8
ongkay <bisnismilyarandollar@gmail.com>
2023-02-25 17:24:08 +0700

    install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className='bg-blue-700 font-bold text-red'>apa hayo</h1>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       </header>
     </div>
   );
