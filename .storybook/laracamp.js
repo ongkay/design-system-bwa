637a31b61c984516a058c3221cdf2d255ff498f6
ongkay <bisnismilyarandollar@gmail.com>
2023-02-26 17:18:11 +0700

    refactoring

---
+++ .storybook/laracamp.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'BWADSREACT',
+  brandUrl: '/',
+  brandImage: '/logo.png',
+  brandTarget: '_self',
+});
---
+++ dist/App.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+function App() {
+    return ((0, jsx_runtime_1.jsx)("div", { className: "App", children: (0, jsx_runtime_1.jsxs)("header", { className: "App-header", children: [(0, jsx_runtime_1.jsx)("img", { src: "/images/image-2.jpg", alt: "logo" }), (0, jsx_runtime_1.jsx)("img", { src: "/logo.png", alt: "logo" }), (0, jsx_runtime_1.jsxs)("p", { children: ["Edit ", (0, jsx_runtime_1.jsx)("code", { children: "src/App.tsx" }), " and save to reload."] }), (0, jsx_runtime_1.jsx)("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer", children: "Learn React" }), (0, jsx_runtime_1.jsx)("h1", { className: "font-bold bg-blue-700 text-red", children: "apa hayo" }), (0, jsx_runtime_1.jsx)("h1", { className: "text-3xl font-bold underline", children: "Hello world!" })] }) }));
+}
+exports.default = App;
